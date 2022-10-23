b4590a41ac538abc5ab4fe304d6b7cf6ecbdc559
longhainguyen <100198025+longhainguyen@users.noreply.github.com>
2022-10-24 00:44:44 +0700

    update move no

--- src/uet/oop/bomberman/intelligent/MoveNo.java
+++ src/uet/oop/bomberman/intelligent/MoveNo.java
@@ -14,10 +14,27 @@ public class MoveNo extends MoveRandom {
 
     public MoveNo() {
         random = new Random();
+        start = System.currentTimeMillis()/1000;
     }
 
     @Override
     public void moveRandom(Entity entity, Collision collision, Rect rect) {
+        long temp = 0;
         super.moveRandom(entity, collision, rect);
+        if(Speed_random > 0) {
+            temp = end - start;
+            end = System.currentTimeMillis() / 1000;
+            if(temp > 5) {
+                Speed_random = 0;
+                start = System.currentTimeMillis() / 1000;
+            }
+        }else {
+            end = System.currentTimeMillis() / 1000;
+            temp = end - start;
+            if(temp > 5) {
+                Speed_random = 2;
+                start = System.currentTimeMillis() / 1000;
+            }
+        }
     }
 }
